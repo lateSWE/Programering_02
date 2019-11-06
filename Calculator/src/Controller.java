8ab41bea3fc3157cbfd16af69d7698e2b44188e0
lateSWE <35782260+lateSWE@users.noreply.github.com>
2019-11-06 11:58:40 +0100

    made it function

--- Calculator/src/Controller.java
+++ Calculator/src/Controller.java
@@ -1,99 +1,158 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Window;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Controller {
 
     @FXML
-    public TextField text_field;
+    private TextField text_field;
 
     @FXML
-    private void btn_1(ActionEvent event){ System.out.println("Button: \"btn_1\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_1(ActionEvent event){
+        System.out.println("Button: \"btn_1\" was pressed");
+        text_field.setText(text_field.getText() + "1");
     }
     @FXML
-    private void btn_2(ActionEvent event){ System.out.println("Button: \"btn_2\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_2(ActionEvent event){
+        System.out.println("Button: \"btn_2\" was pressed");
+        text_field.setText(text_field.getText() + "2");
     }
     @FXML
-    private void btn_3(ActionEvent event){ System.out.println("Button: \"btn_3\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_3(ActionEvent event){
+        System.out.println("Button: \"btn_3\" was pressed");
+        text_field.setText(text_field.getText() + "3");
     }
     @FXML
-    private void btn_add(ActionEvent event){ System.out.println("Button: \"btn_add\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_add(ActionEvent event){
+        System.out.println("Button: \"btn_add\" was pressed");
+        text_field.setText(text_field.getText() + "+");
     }
     @FXML
-    private void btn_sin(ActionEvent event){ System.out.println("Button: \"btn_sin\" was pressed");
-        text_field.setText(text_field.getText() + "");
-    }
-
-
-    @FXML
-    private void btn_4(ActionEvent event){ System.out.println("Button: \"btn_4\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_sin(ActionEvent event){
+        System.out.println("Button: \"btn_sin\" was pressed");
+        text_field.setText(text_field.getText() + "sin(");
     }
     @FXML
-    private void btn_5(ActionEvent event){ System.out.println("Button: \"btn_5\" was pressed");
-        text_field.setText(text_field.getText() + "");
-    }
-    @FXML
-    private void btn_6(ActionEvent event){ System.out.println("Button: \"btn_6\" was pressed");
-        text_field.setText(text_field.getText() + "");
-    }
-    @FXML
-    private void btn_sub(ActionEvent event){ System.out.println("Button: \"btn_sub\" was pressed");
-        text_field.setText(text_field.getText() + "");
-    }
-    @FXML
-    private void btn_cos(ActionEvent event){ System.out.println("Button: \"btn_cos\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_C(ActionEvent event){
+        System.out.println("Button: \"btn_sin\" was pressed");
+        text_field.setText("");
     }
 
 
     @FXML
-    private void btn_7(ActionEvent event){ System.out.println("Button: \"btn_7\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_4(ActionEvent event){
+        System.out.println("Button: \"btn_4\" was pressed");
+        text_field.setText(text_field.getText() + "4");
     }
     @FXML
-    private void btn_8(ActionEvent event){ System.out.println("Button: \"btn_8\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_5(ActionEvent event){
+        System.out.println("Button: \"btn_5\" was pressed");
+        text_field.setText(text_field.getText() + "5");
     }
     @FXML
-    private void btn_9(ActionEvent event){ System.out.println("Button: \"btn_9\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_6(ActionEvent event){
+        System.out.println("Button: \"btn_6\" was pressed");
+        text_field.setText(text_field.getText() + "6");
     }
     @FXML
-    private void btn_multi(ActionEvent event){ System.out.println("Button: \"btn_multi\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_sub(ActionEvent event){
+        System.out.println("Button: \"btn_sub\" was pressed");
+        text_field.setText(text_field.getText() + "-");
     }
     @FXML
-    private void btn_percent(ActionEvent event){ System.out.println("Button: \"btn_percent\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_cos(ActionEvent event){
+        System.out.println("Button: \"btn_cos\" was pressed");
+        text_field.setText(text_field.getText() + "cos(");
+    }
+    @FXML
+    private void btn_tan(ActionEvent event){
+        System.out.println("Button: \"btn_sin\" was pressed");
+        text_field.setText(text_field.getText() + "tan(");
     }
 
 
     @FXML
-    private void btn_C(ActionEvent event){ System.out.println("Button: \"btn_C\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_7(ActionEvent event){
+        System.out.println("Button: \"btn_7\" was pressed");
+        text_field.setText(text_field.getText() + "7");
     }
     @FXML
-    private void btn_0(ActionEvent event){ System.out.println("Button: \"btn_0\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_8(ActionEvent event){
+        System.out.println("Button: \"btn_8\" was pressed");
+        text_field.setText(text_field.getText() + "8");
     }
     @FXML
-    private void btn_equals(ActionEvent event){ System.out.println("Button: \"btn_equals\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_9(ActionEvent event){
+        System.out.println("Button: \"btn_9\" was pressed");
+        text_field.setText(text_field.getText() + "9");
     }
     @FXML
-    private void btn_divide(ActionEvent event){ System.out.println("Button: \"btn_divide\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_multi(ActionEvent event){
+        System.out.println("Button: \"btn_multi\" was pressed");
+        text_field.setText(text_field.getText() + "*");
     }
     @FXML
-    private void btn_root(ActionEvent event){ System.out.println("Button: \"btn_root\" was pressed");
-        text_field.setText(text_field.getText() + "");
+    private void btn_percent(ActionEvent event){
+        System.out.println("Button: \"btn_percent\" was pressed");
+        text_field.setText(text_field.getText() + "%");
+    }
+    @FXML
+    private void btn_par1(ActionEvent event){
+        System.out.println("Button: \"btn_sin\" was pressed");
+        text_field.setText(text_field.getText() + "(");
+    }
+
+
+    @FXML
+    private void btn_comma(ActionEvent event){
+        System.out.println("Button: \"btn_comma\" was pressed");
+        text_field.setText(text_field.getText() + ",");
+    }
+    @FXML
+    private void btn_0(ActionEvent event){
+        System.out.println("Button: \"btn_0\" was pressed");
+        text_field.setText(text_field.getText() + "0");
+    }
+    @FXML
+    private void btn_equals(ActionEvent event){
+        System.out.println("Button: \"btn_equals\" was pressed");
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        String calc = replace(text_field.getText());
+        try {
+            text_field.setText(engine.eval(calc).toString());
+            System.out.println(calc);
+            System.out.println("Equals: " + engine.eval(calc).toString());
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void btn_divide(ActionEvent event){
+        System.out.println("Button: \"btn_divide\" was pressed");
+        text_field.setText(text_field.getText() + "/");
+    }
+    @FXML
+    private void btn_root(ActionEvent event){
+        System.out.println("Button: \"btn_root\" was pressed");
+        text_field.setText(text_field.getText() + "sqrt(");
+    }
+    @FXML
+    private void btn_par2(ActionEvent event){
+        System.out.println("Button: \"btn_sin\" was pressed");
+        text_field.setText(text_field.getText() + ")");
+    }
+
+
+    private String replace(String in){
+        in = in.replaceAll("sqrt", "Math.sqrt ");
+        in = in.replaceAll("sin", "Math.sin ");
+        in = in.replaceAll("cos", "Math.cos ");
+        in = in.replaceAll("tan", "Math.tan ");
+        return in;
     }
 }
--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -9,7 +9,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
         primaryStage.setTitle("Calculator");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 290, 215));
 
         primaryStage.show();
     }
